ce5b27270ee1f727092470862143ceea20840225
Luis A Chee <alfchee@gmail.com>
2020-12-30 17:40:24 -0600

    Nuxt init!

---
+++ app/pages/dashboard/episodes.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Episodes</h1>
+</template>
+
+<script>
+export default {
+  name: 'Episodes',
+}
+</script>
+
+<style></style>
---
+++ app/pages/dashboard/podcast/edit.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Edit Podcast Settings</h1>
+</template>
+
+<script>
+export default {
+  name: 'PodcastSettings',
+}
+</script>
+
+<style></style>
