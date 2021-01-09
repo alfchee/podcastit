2f362e55d1b23ab7c186f5f8678c84f852ab4409
Luis A Chee <alfchee@gmail.com>
2021-01-09 11:00:47 -0600

    Creating endpoint for users

---
+++ api/src/user/user.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Get, Param, Post, Body, Put } from '@nestjs/common';
+import { UserService } from '../user/user.service';
+import { Prisma, User as UserModel } from '@prisma/client';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get(':id')
+  async getUserBySubId(@Param('id') id: string): Promise<UserModel> {
+    return this.userService.user({ subid: id });
+  }
+
+  @Post()
+  async createUser(
+    @Body() userData: Prisma.UserCreateInput,
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
+
+  @Put(':id')
+  async updateUser(
+    @Param('id') id: string,
+    @Body() data: Prisma.UserUpdateInput,
+  ): Promise<UserModel> {
+    return this.userService.updateUser({
+      where: { subid: id },
+      data: data,
+    });
+  }
+}
